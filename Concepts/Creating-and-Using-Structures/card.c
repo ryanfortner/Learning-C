ce53ff3dc6600747a4da4b91af4c5225d52c9b7a
Ryan Fortner <ryankfortner@gmail.com>
2022-02-17 12:58:40 -0500

    Create card.c

---
+++ Concepts/Creating-and-Using-Structures/card.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    struct Card card;
+
+    printf("enum Suit is %lu bytes\n", sizeof(enum Suit));
+    printf("enum Face is %lu bytes\n", sizeof(enum Face));
+    printf("int is %lu bytes\n", sizeof(int));
+    printf("bool is %lu bytes\n", sizeof(bool));
+
+    printf("struct Card is %lu bytes\n", sizeof(struct Card));
+    printf("card is %lu bytes\n", sizeof(card));
+
+    return 0;
+}
+
+enum Suit {
+    club = 1,
+    diamond,
+    heart,
+    spade
+};
+
+enum Face {
+    one = 1,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    jack,
+    queen,
+    king,
+    ace
+};
+
+struct Card {
+    enum Suit suit;
+    int suitValue;
+    enum Face face;
+    int faceValue;
+    bool isWild;
+};
+
+struct Card card;
